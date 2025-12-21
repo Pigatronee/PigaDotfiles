227b8f5c32e7ae6444a3f2ee28bc9e2a6838f546
Pigatronee <james@pigatronee.com>
2025-12-21 00:20:50 +0000

    updated install.sh

--- AutoInstallDotfiles/install.sh
+++ AutoInstallDotfiles/install.sh
@@ -1,110 +1,50 @@
 #!/bin/bash
 
-# 
+# Variablesz
+packages_requried=("hyprland" "waybar" "waypaper" "wlogout" "xsettingsd" "rofi" "fuzzel" "kitt" "python-pywal16-git")
+packages_extra=("cava" "pavucontrol" "nwg-look" "eww" "fuzzel" "swww" "xdg-desktop-portal-hyprland" "htop" "vesktop" "firefox" "godot" "nvim" "nextcloud" "steam" "ly" "nautilus" "playerctl" "pamixer" "dotool")
 
-# Paths
-DOTFILES_PATH=$(pwd)/my_configs
-CONFIG_PATH=$HOME/.config
-CORE_PACKAGES_FILE="packages.txt"
-EXTRA_PACKAGES_FILE="packages_extra.txt"
+combined_packages=("${packages_requried[@]}" "${packages_extra[@]}")
 
-# Helper Functions
-ask_question() {
-    local msg=$1
-    local options=$2
-    echo "$msg"
-    select answer in $options; do
-        case $answer in
-            1) return 0 ;;
-            2) return 1 ;;
-            *) echo "Invalid choice, please choose again." ;;
-        esac
-    done
-}
-
-install_packages() {
-    echo "About to install packages from $CORE_PACKAGES_FILE and $EXTRA_PACKAGES_FILE..."
-    
-    # Read core packages
-    PACKAGES=$(cat $CORE_PACKAGES_FILE)
-    if [ "$1" != "required_only" ]; then
-        # Read extra packages
-        PACKAGES="$PACKAGES $(cat $EXTRA_PACKAGES_FILE)"
-    fi
-    
-    echo "Packages to install: $PACKAGES"
-    
-    ask_question "Would you like to continue with the installation?" "[Yes, No]"
-    if [ $? -eq 0 ]; then
-        yay -S --needed $PACKAGES
-    else
-        echo "Aborting package installation."
-    fi
-}
-
-merge_dotfiles() {
-    echo "About to merge dotfiles from $DOTFILES_PATH to $CONFIG_PATH..."
-    for folder in $(ls $DOTFILES_PATH); do
-        folder_path="$DOTFILES_PATH/$folder"
-        target_path="$CONFIG_PATH/$folder"
-        
-        if [ -d "$target_path" ]; then
-            echo "$folder already exists in $CONFIG_PATH."
-            ask_question "Do you want to delete it and copy the new one?" "[Yes, No]"
-            if [ $? -eq 0 ]; then
-                echo "Deleting existing $folder and copying new dotfiles..."
-                rm -rf "$target_path"
-                cp -r "$folder_path" "$CONFIG_PATH/"
-            else
-                echo "Skipping $folder."
-            fi
-        else
-            echo "Copying $folder to $CONFIG_PATH..."
-            cp -r "$folder_path" "$CONFIG_PATH/"
-        fi
-    done
-}
-
-replace_dotfiles() {
-    echo "Warning: Replacing dotfiles can break things. Are you sure?"
-    read -p "Type 'I am really stupid' to continue: " confirmation
-    if [ "$confirmation" == "I am really stupid" ]; then
-        echo "Replacing all dotfiles..."
-        rm -rf $CONFIG_PATH/*
-        cp -r $DOTFILES_PATH/* $CONFIG_PATH/
-    else
-        echo "Aborted dotfile replacement."
-    fi
-}
-
-# Main Menu
-echo "WARNING WARNING!!!!!!!!!!!!!!!!!\n DO NOT USE THIS SCRIPT!!!!!!!\n IT IS LLM SLOP AND I HAVE NOT MADE IT PROPERLY YET!!!!!!\n WHY IS IT HERE?? IDK!!! JUST DONT RUN IT PLS@!!!!!!!!!"
+# Prompt
+clear
 echo "What would you like to do?"
-select choice in "Install packages" "Install dotfiles" "Quit"; do
-    case $choice in
-        "Install packages")
-            ask_question "Install all required packages or only the minimum?" "[All, Minimum]"
-            if [ $? -eq 0 ]; then
-                install_packages
-            else
-                install_packages "required_only"
-            fi
-            ;;
-        "Install dotfiles")
-            ask_question "Would you like to replace your entire .config or merge it?" "[Replace, Merge]"
-            if [ $? -eq 0 ]; then
-                replace_dotfiles
-            else
-                merge_dotfiles
-            fi
-            ;;
-        "Quit")
-            echo "Exiting..."
-            break
-            ;;
-        *)
-            echo "Invalid choice, please try again."
-            ;;
-    esac
-done
+echo "[1]: Install dependencies"
+echo "[2]: Install dotfiles"
+echo -n "[1]/[2]: "
+read -r answer
+
+install_dependencies_cli() {
+	clear 
+	echo "Do you want to install the extremely recommended extra packages too?"
+	echo -n "[y]/[n]: "
+	read -r answer
+	
+	if [ "$answer" == "y" ]; then
+		echo "Ok you made the right choice :p"
+		yay -S --needed "${combined_packages[@]}"
+	elif [ "$answer" == "n" ]; then
+		echo "bruh ok"
+		yay -S --needed "${packages_requried[@]}"
+	else 
+		echo "Invalid option."
+		install_dependencies_cli
+	fi
+
+}
+
+
+# MAIN
+
+# Check what to do 
+if [ "$answer" -eq 1 ]; then
+	echo "Installing dependencies..."
+	install_dependencies_cli
+elif [ "$answer" -eq 2 ]; then
+	echo "Installing dotfiles..."
+else 
+	echo "Invalid option."
+fi
+
+
 
