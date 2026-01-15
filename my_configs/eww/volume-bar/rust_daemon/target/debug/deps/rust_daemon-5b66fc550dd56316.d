30ab5926178de8b023b58a9c6443b92273c376fc
PigObsessedGameDev <jamesjimmybob96@gmail.com>
2026-01-15 11:46:38 +0000

    Almost finished rust daemon

---
+++ my_configs/eww/volume-bar/rust_daemon/target/debug/deps/rust_daemon-5b66fc550dd56316.d
@@ -0,0 +1,5 @@
+/Users/apple/Documents/GitHub/PigaDotfiles/my_configs/eww/volume-bar/rust_daemon/target/debug/deps/rust_daemon-5b66fc550dd56316.d: src/main.rs
+
+/Users/apple/Documents/GitHub/PigaDotfiles/my_configs/eww/volume-bar/rust_daemon/target/debug/deps/librust_daemon-5b66fc550dd56316.rmeta: src/main.rs
+
+src/main.rs:
