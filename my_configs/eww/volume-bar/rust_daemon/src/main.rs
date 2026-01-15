8333af2ff6483cb72eb2a33a8fd190d392c35f96
Pigatronee <james@pigatronee.com>
2026-01-15 00:43:57 +0000

    rust daemon for volume bar is getting therer

--- my_configs/eww/volume-bar/rust_daemon/src/main.rs
+++ my_configs/eww/volume-bar/rust_daemon/src/main.rs
@@ -1,36 +1,38 @@
-use std::io::{BufRead, BufReader};
-use std::process::{Command, Stdio};
+use std::process::Command;
 
-fn get_volume() -> i32 {
-    // Placeholder for your actual volume-getting logic
-    50
+fn get_volume() -> u8 {
+    let result = Command::new("pamixer")
+        .arg("--get-volume")
+        .output()
+        .expect("Failed to get volume");
+   let stdout = String::from_utf8(result.stdout)
+        .expect("output isn't a u8");
+    stdout
+        .trim()
+        .parse::<u8>()
+        .expect("Couldn't parse the stdout")
 }
 
-fn update_volume(vol: i32) {
-    println!("Updating volume to {}", vol);
+fn _update_volume(volume_level: u8, bar_open: &mut bool) {
+    Command::new("eww")
+        .arg("update")
+        .arg(format!("volume={}",volume_level));
+    _open_eww(bar_open);
+}
+
+fn _open_eww(bar_open: &mut bool) {
+    if *bar_open == true{return}
+    Command::new("eww")
+        .arg("open")
+        .arg("volume-bar")
+        .output()
+        .expect("Failed to open volume bar");
+    *bar_open = true;
 }
 
 fn main() {
-    let mut current_volume: i32 = 0;
-
-    let mut proc = Command::new("pactl")
-        .arg("subscribe")
-        .stdout(Stdio::piped())
-        .spawn()
-        .expect("failed to start pactl");
-
-    let stdout = proc.stdout.take().expect("Failed to capture stdout");
-    let reader = BufReader::new(stdout);
-
-    for line in reader.lines() {
-        let line = line.expect("Failed to read line");
-        if line.to_lowercase().contains("sink") {
-            let vol = get_volume();
-            if vol != current_volume {
-                update_volume(vol);
-                current_volume = vol;
-            }
-        }
-    }
+    let volume: u8 = get_volume();
+    let mut bar_open: bool = false;
+    println!("Volume changed, Updating bar.");
+    _update_volume(volume, &mut bar_open);
 }
-
