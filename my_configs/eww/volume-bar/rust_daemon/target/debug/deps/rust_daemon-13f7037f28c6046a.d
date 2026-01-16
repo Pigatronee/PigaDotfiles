b349d7f4c296c70cf9e0550e693cd980801dedf3
Pigatronee <james@pigatronee.com>
2026-01-16 03:10:42 +0000

    Finished rust daemon

--- my_configs/eww/volume-bar/rust_daemon/target/debug/deps/rust_daemon-13f7037f28c6046a.d
+++ my_configs/eww/volume-bar/rust_daemon/target/debug/deps/rust_daemon-13f7037f28c6046a.d
@@ -1,5 +1,5 @@
-/home/Pigatronee/.config/eww/volume-bar/rust_daemon/target/debug/deps/rust_daemon-13f7037f28c6046a.d: src/main.rs
+/home/Pigatronee/Documents/github/PigaDotfiles/my_configs/eww/volume-bar/rust_daemon/target/debug/deps/rust_daemon-13f7037f28c6046a.d: src/main.rs
 
-/home/Pigatronee/.config/eww/volume-bar/rust_daemon/target/debug/deps/librust_daemon-13f7037f28c6046a.rmeta: src/main.rs
+/home/Pigatronee/Documents/github/PigaDotfiles/my_configs/eww/volume-bar/rust_daemon/target/debug/deps/librust_daemon-13f7037f28c6046a.rmeta: src/main.rs
 
 src/main.rs:
