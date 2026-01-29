1bd8b4c62cc93bd529e6326d7042167a5049fbf2
Pigatronee <james@pigatronee.com>
2026-01-29 17:21:32 +0000

    waypaper conf

---
+++ my_configs/eww/volume-bar/rust_daemon/target/debug/deps/cancellable_timer-ec81907a6101d1e2.d
@@ -0,0 +1,5 @@
+/home/Pigatronee/.config/eww/volume-bar/rust_daemon/target/debug/deps/cancellable_timer-ec81907a6101d1e2.d: /home/Pigatronee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cancellable-timer-0.1.0/src/lib.rs
+
+/home/Pigatronee/.config/eww/volume-bar/rust_daemon/target/debug/deps/libcancellable_timer-ec81907a6101d1e2.rmeta: /home/Pigatronee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cancellable-timer-0.1.0/src/lib.rs
+
+/home/Pigatronee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/cancellable-timer-0.1.0/src/lib.rs:
---
+++ my_configs/eww/volume-bar/rust_daemon/target/debug/deps/slab-04e9f9d1ce5d238c.d
@@ -0,0 +1,6 @@
+/home/Pigatronee/.config/eww/volume-bar/rust_daemon/target/debug/deps/slab-04e9f9d1ce5d238c.d: /home/Pigatronee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/slab-0.4.11/src/lib.rs /home/Pigatronee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/slab-0.4.11/src/builder.rs
+
+/home/Pigatronee/.config/eww/volume-bar/rust_daemon/target/debug/deps/libslab-04e9f9d1ce5d238c.rmeta: /home/Pigatronee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/slab-0.4.11/src/lib.rs /home/Pigatronee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/slab-0.4.11/src/builder.rs
+
+/home/Pigatronee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/slab-0.4.11/src/lib.rs:
+/home/Pigatronee/.cargo/registry/src/index.crates.io-1949cf8c6b5b557f/slab-0.4.11/src/builder.rs:
